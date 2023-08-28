1d8e422d74b48d3df9f9260698a169e6b7d00118
PhilKorte <45069064+PhilKorte@users.noreply.github.com>
2023-08-28 11:48:50 +0000

    quotes

--- AWS_Network/main.tf
+++ AWS_Network/main.tf
@@ -6,7 +6,7 @@ apk add jq
 curl -o sg-cli https://raw.githubusercontent.com/StackGuardian/sg-cli/982af6d83e889c012d648cbf89896b9aa2f64695/shell/sg-cli
 chmod +x sg-cli
 
-./sg-cli stack create --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --run --output-json --wait --patch-payload '{"ResourceName": "${var.workspace_id}_${var.project_id}_${var.description}","TemplatesConfig": {"templates": [{"ResourceName": "aws-iam-ipam-role"},{"VCSConfig":{"iacInputData":{"data":{"shortDescription":${var.description},"networkSize":${var.size}}}}},{"ResourceName": "aws-iam-avx-role"},{"VCSConfig":{"iacInputData":{"data":{"gwSize":${var.gateway},"account":${var.account}}}}}]}}' -- payload.json > output.txt
+./sg-cli stack create --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --run --output-json --wait --patch-payload '{"ResourceName": "${var.workspace_id}_${var.project_id}_${var.description}","TemplatesConfig": {"templates": [{"ResourceName": "aws-iam-ipam-role"},{"VCSConfig":{"iacInputData":{"data":{"shortDescription":"${var.description}","networkSize":${var.size}}}}},{"ResourceName": "aws-iam-avx-role"},{"VCSConfig":{"iacInputData":{"data":{"gwSize":"${var.gateway}","account":"${var.account}"}}}}]}}' -- payload.json > output.txt
 
 if grep -qi 'Stack name not unique' output.txt
 then
