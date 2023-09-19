c29289d29a9d287f5552054e65fee0283eafae06
PhilKorte <45069064+PhilKorte@users.noreply.github.com>
2023-09-19 12:16:27 +0000

    test without output.txt

--- AWS_Network/main.tf
+++ AWS_Network/main.tf
@@ -6,16 +6,14 @@ apk add jq
 curl -o sg-cli https://raw.githubusercontent.com/StackGuardian/sg-cli/982af6d83e889c012d648cbf89896b9aa2f64695/shell/sg-cli
 chmod +x sg-cli
 
-./sg-cli stack create --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --run --output-json --wait --patch-payload '{"ResourceName": "${var.workspace_id}_${var.project_id}_${var.description}","TemplatesConfig": {"templates": [{"ResourceName": "aws-iam-ipam-role"},{"VCSConfig":{"iacInputData":{"data":{"shortDescription":"${var.description}","networkSize":${var.size}}}}},{"ResourceName": "aws-iam-avx-role"},{"VCSConfig":{"iacInputData":{"data":{"gwSize":"${var.gateway}","account":"${var.account}"}}}}]}}' -- payload.json > output.txt
+./sg-cli stack create --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --run --output-json --wait --patch-payload '{"ResourceName": "${var.workspace_id}_${var.project_id}_${var.description}","TemplatesConfig": {"templates": [{"ResourceName": "aws-iam-ipam-role"},{"VCSConfig":{"iacInputData":{"data":{"shortDescription":"${var.description}","networkSize":${var.size}}}}},{"ResourceName": "aws-iam-avx-role"},{"VCSConfig":{"iacInputData":{"data":{"gwSize":"${var.gateway}","account":"${var.account}"}}}}]}}' -- payload.json
 
-if grep -qi 'Stack name not unique' output.txt
+if gre p -qi 'Stack name not unique'
 then
   echo "Stack Name: ${var.workspace_id}_${var.project_id}_${var.description} already exists. Running APPLY on existing Stack"
-  ./sg-cli stack apply --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --stack-id ${var.workspace_id}_${var.project_id}_${var.description} --output-json --wait > output.txt
+  ./sg-cli stack apply --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --stack-id ${var.workspace_id}_${var.project_id}_${var.description} --output-json --wait
 fi
 
-cat output.txt
-rm output.txt
 EOF
   }
 }
