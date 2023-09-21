bdf08c5cb507211780a031a62465196fc7f18669
PhilKorte <45069064+PhilKorte@users.noreply.github.com>
2023-09-21 09:01:57 +0000

    init

---
+++ Azure_Network/main.tf
@@ -0,0 +1,21 @@
+resource "null_resource" "sg_integration" {
+provisioner "local-exec" {
+    command = <<EOF
+apk add curl
+apk add jq
+curl -o sg-cli https://raw.githubusercontent.com/StackGuardian/sg-cli/main/shell/sg-cli
+chmod +x sg-cli
+
+./sg-cli stack create --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --run --output-json --wait --patch-payload '{"ResourceName": "${var.workspace_id}_${var.project_id}_${var.description}","TemplatesConfig": {"templates": [{"ResourceName": "aws-iam-ipam-role"},{"VCSConfig":{"iacInputData":{"data":{"shortDescription":"${var.description}","networkSize":${var.size}}}}},{"EnvironmentVariables":[{"config": {"secretId": "/secrets/Azure_Client_ID","varName": "ARM_CLIENT_ID"},"kind": "VAULT_SECRET"},{„config": {"secretId": "/secrets/Azure_Client_Secret","varName": "ARM_CLIENT_SECRET"},"kind": "VAULT_SECRET"},{"config": {"secretId": "/secrets/Azure_Tenant_ID","varName": "ARM_TENANT_ID"},"kind": "VAULT_SECRET" },{"config": {"textValue": „${var.account}“,"varName": "ARM_SUBSCRIPTION_ID"},„kind": "PLAIN_TEXT"},{"config": {"secretId": "/secrets/Aviatrix_Controller_IP","varName": "AVIATRIX_CONTROLLER_IP"},"kind": "VAULT_SECRET"},{"config": {"secretId": "/secrets/Aviatrix_Controller_Username","varName": "AVIATRIX_USERNAME"}, "kind": "VAULT_SECRET"},{"config": {"secretId": "/secrets/Aviatrix_Controller_Password","varName": "AVIATRIX_PASSWORD" },"kind": "VAULT_SECRET"}],"VCSConfig":{"iacInputData":{"data":{"gwSize":"${var.gateway}","subscriptionID":"${var.account}"}}}}]}}' -- payload.json > output.txt
+
+if grep -qi 'Stack name not unique' output.txt
+then
+  echo "Stack Name: ${var.workspace_id}_${var.project_id}_${var.description} already exists. Running APPLY on existing Stack"
+  ./sg-cli stack apply --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --stack-id ${var.workspace_id}_${var.project_id}_${var.description} --output-json --wait > output.txt
+fi
+
+cat output.txt
+rm output.txt
+EOF
+  }
+}
---
+++ Azure_Network/variables.tf
@@ -0,0 +1,40 @@
+variable "sg_organization" {
+  type        = string
+  description = "StackGuardian Organization name"
+  nullable    = false
+}
+variable "sg_workflow_group" {
+  type        = string
+  description = "StackGuardian Workflow Group name"
+  nullable    = false
+}
+variable "workspace_id" {
+  type        = string
+  description = "meshcloud Customer ID"
+  nullable    = false
+}
+variable "project_id" {
+  type        = string
+  description = "meshcloud Project ID"
+  nullable    = false
+}
+variable "account" {
+  type = string
+  description = "Account ID of the Azure Subscription"
+  nullable = false
+}
+variable "size" {
+  type = number
+  description = "Subnetmask of the VPC"
+  nullable = false
+}
+variable "description" {
+    type = string
+    description = "Short description for the VPC"
+    nullable = false
+}
+variable "gateway" {
+    type = string
+    description = "AWS Instance Type for the GW"
+    nullable = false
+}
