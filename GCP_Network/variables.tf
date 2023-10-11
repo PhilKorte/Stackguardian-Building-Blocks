73dfd32b0235519eea35f662f6963c29da4fd0a6
PhilKorte <45069064+PhilKorte@users.noreply.github.com>
2023-10-11 12:50:41 +0000

    init gcp

---
+++ GCP_Network/variables.tf
@@ -0,0 +1,45 @@
+variable "sg_organization" {
+  type        = string
+  description = "StackGuardian Organization name"
+  nullable    = false
+}
+variable "sg_workflow_group" {
+  type        = string
+  description = "StackGuardian Workflow Group name"
+  nullable    = false
+}
+variable "workspace_id" {
+  type        = string
+  description = "meshcloud Customer ID"
+  nullable    = false
+}
+variable "project_id" {
+  type        = string
+  description = "meshcloud Project ID"
+  nullable    = false
+}
+variable "account" {
+  type = string
+  description = "Account ID of the GCP Project"
+  nullable = false
+}
+variable "size" {
+  type = number
+  description = "Subnetmask of the VPC"
+  nullable = false
+}
+variable "description" {
+    type = string
+    description = "Short description for the VPC"
+    nullable = false
+}
+variable "gateway" {
+    type = string
+    description = "GCP Instance Type for the GW"
+    nullable = false
+}
+variable "region" {
+    type = string
+    description = "GCP Region for the VPC"
+    nullable = false
+}
