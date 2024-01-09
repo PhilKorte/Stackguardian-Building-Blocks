e5fdcf0fb790a0fd0ecd384aa8d24fff517b0e94
PhilKorte <45069064+PhilKorte@users.noreply.github.com>
2024-01-09 15:30:07 +0100

    Update main.tf

--- AWS_Network/main.tf
+++ AWS_Network/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "sg_integration" {
 provisioner "local-exec" {
     command = <<EOF
-curl -o sg-cli https://raw.githubusercontent.com/StackGuardian/sg-cli/main/shell/sg-cli
+curl -o sg-cli https://raw.githubusercontent.com/StackGuardian/sg-cli/main/sg-cli
 chmod +x sg-cli
 
 ./sg-cli stack create --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --run --output-json --wait --patch-payload '{"ResourceName": "${var.workspace_id}_${var.project_id}_${var.description}","TemplatesConfig": {"templates": [{"ResourceName": "aws-iam-ipam-role"},{"VCSConfig":{"iacInputData":{"data":{"shortDescription":"${var.description}","networkSize":${var.size}}}}},{"VCSConfig":{"iacInputData":{"data":{"accountID":"${var.account}"}}}},{"VCSConfig":{"iacInputData":{"data":{"gwSize":"${var.gateway}","account":"${var.account}"}}}}]}}' -- payload.json > output.txt
