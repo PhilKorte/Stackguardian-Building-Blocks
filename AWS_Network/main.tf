155b44d2f4fcbea71426a623eb28b88040c42200
PhilKorte <45069064+PhilKorte@users.noreply.github.com>
2024-04-15 11:44:41 +0000

    was gelöscht

--- AWS_Network/main.tf
+++ AWS_Network/main.tf
@@ -18,7 +18,7 @@ resource "stackguardian_stack" "network_stack" {
         "templateGroupId": "/rheinenergie/AWS_Network:4",
         "templates": [
             {
-                "ResourceName": "aws-iam-ipam-role",
+                "ResourceName": "aws-iam-ipam-role", 
                 "Description": "Role for accessing AWS IPAM",
                 "Tags": [],
                 "Approvers": [],
@@ -316,30 +316,3 @@ resource "stackguardian_stack" "network_stack" {
 } 
   )
 }
-
-
-
-
-
-
-
-
-/* resource "null_resource" "sg_integration" {
-provisioner "local-exec" {
-    command = <<EOF
-curl -o sg-cli https://raw.githubusercontent.com/StackGuardian/sg-cli/main/sg-cli
-chmod +x sg-cli
-
-./sg-cli stack create --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --run --output-json --wait --patch-payload '{"ResourceName": "${var.workspace_id}_${var.project_id}_${var.description}","TemplatesConfig": {"templates": [{"ResourceName": "aws-iam-ipam-role"},{"VCSConfig":{"iacInputData":{"data":{"shortDescription":"${var.description}","networkSize":${var.size}}}}},{"VCSConfig":{"iacInputData":{"data":{"accountID":"${var.account}"}}}},{"VCSConfig":{"iacInputData":{"data":{"gwSize":"${var.gateway}","account":"${var.account}"}}}}]}}' -- payload.json > output.txt
-
-if grep -qi 'Stack name not unique' output.txt
-then
-  echo "Stack Name: ${var.workspace_id}_${var.project_id}_${var.description} already exists. Running APPLY on existing Stack"
-  ./sg-cli stack apply --org ${var.sg_organization} --workflow-group ${var.sg_workflow_group} --stack-id ${var.workspace_id}_${var.project_id}_${var.description} --output-json --wait > output.txt
-fi
-
-cat output.txt
-rm output.txt
-EOF
-  }
-} */
