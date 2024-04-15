e6f63f2eccd4056303e8a6f2c21347b16d49f368
PhilKorte <45069064+PhilKorte@users.noreply.github.com>
2024-04-15 11:39:27 +0000

    variables

--- AWS_Network/variables.tf
+++ AWS_Network/variables.tf
@@ -1,8 +1,3 @@
-variable "sg_organization" {
-  type        = string
-  description = "StackGuardian Organization name"
-  nullable    = false
-}
 variable "sg_workflow_group" {
   type        = string
   description = "StackGuardian Workflow Group name"
